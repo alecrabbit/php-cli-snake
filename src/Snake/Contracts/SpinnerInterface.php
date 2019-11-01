5aed43081a3c66481fc9da31474244d5c1b3252a
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-11-01 21:25:18 +0200

    Added: interface `SpinnerInterface`

---
+++ src/Snake/Contracts/SpinnerInterface.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit\Snake\Contracts;
+
+interface SpinnerInterface
+{
+    public function spin(): void;
+
+    /**
+     * Returns spinner refresh interval
+     * @return float
+     */
+    public function interval(): float;
+
+    public function begin(): void;
+
+    public function end(): void;
+
+    public function erase(): void;
+
+    public function useStdOut(): void;
+}
