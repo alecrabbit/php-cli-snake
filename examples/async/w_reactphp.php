f4b5ee8de8e25c4c754a91fab30ece4eb4ab6c99
Alec Rabbit <alec.therabbit.git@gmail.com>
2020-12-26 16:22:38 +0200

    Updated: example

--- examples/async/w_reactphp.php
+++ examples/async/w_reactphp.php
@@ -6,8 +6,9 @@ use AlecRabbit\Snake\Contracts\Color;
 use AlecRabbit\Snake\Spinner;
 use React\EventLoop\Factory;
 
-$s = new Spinner(Color::COLOR_16); // Downgrade color mode, default is Color::COLOR_256
-$s->useStdOut(); // optional, use STDOUT instead of STDERR
+$s = new Spinner(); // default is Color::COLOR_256
+//$s = new Spinner(Color::COLOR_16); // you can downgrade color mode to Color::COLOR_16
+//$s->useStdOut(); // optional, use STDOUT instead of STDERR
 
 $loop = Factory::create();
 
@@ -15,22 +16,27 @@ $loop->addPeriodicTimer($s->interval(), static function () use ($s) {
     $s->spin();
 });
 
-// Run only for two seconds
-$loop->addTimer(2, static function () use ($loop) {
+// Run for six seconds
+$loop->addTimer(6, static function () use ($loop) {
     $loop->stop();
 });
 
-$loop->addTimer(0.5, static function () {
+$loop->addTimer(1.5, static function () {
     dots();
 });
 
-$loop->addTimer(0.9, static function () {
+$loop->addTimer(2.9, static function () {
     echo 'Message One: is written to StdOut.' . PHP_EOL;
     dots();
 });
 
-$loop->addTimer(1.7, static function () {
+$loop->addTimer(3.7, static function () {
     echo 'Message Two: is written to StdOut.' . PHP_EOL;
+    dots();
+});
+
+$loop->addTimer(4.9, static function () {
+    echo 'Message Three: is written to StdOut.' . PHP_EOL;
     dots();
 });
 
